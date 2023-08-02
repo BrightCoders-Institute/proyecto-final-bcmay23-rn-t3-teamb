733aaf4d279aae44fb6a40d27be68458ba601396
Alfredo20164920 <apaz20164920@gmail.com>
2023-08-01 18:53:37 -0600

    feat: create navigation configuration

--- App.tsx
+++ App.tsx
@@ -6,21 +6,40 @@
  */
 
 import React from 'react';
-import {SafeAreaView, Text, useColorScheme} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+const MainStack = createBottomTabNavigator();
+const LoginStack = createNativeStackNavigator();
+
+function LoginLayout() {
+  return (
+    <LoginStack.Navigator>
+      {/* <LoginStack.Screen name="Main" component={LoginHomeScreen} /> */}
+      {/* <LoginStack.Screen name="SignIn" component={SignInScreen} /> */}
+      {/* <LoginStack.Screen name="SignUp" component={SignUpScreen} /> */}
+    </LoginStack.Navigator>
+  )
+}
+
+function MainLayout() {
+  return (
+    <MainStack.Navigator>
+      {/* <MainStack.Screen name="Home" component={HomeScreen} /> */}
+      {/* <MainStack.Screen name="Settings" component={SettingsScreen} /> */}
+    </MainStack.Navigator>
+  );
+}
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <Text>Hola mundo</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <MainStack.Navigator>
+        {/* <MainStack.Screen name='test' component={test}/> */}
+        <MainStack.Screen name='Main' component={MainLayout} options={{headerShown: false}}/>
+      </MainStack.Navigator>
+    </NavigationContainer>
   );
 }
 
