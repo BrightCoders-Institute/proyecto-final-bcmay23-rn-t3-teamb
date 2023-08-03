c80663775f47a49659111ec7552d543185f87bb6
Alfredo20164920 <apaz20164920@gmail.com>
2023-08-02 18:11:47 -0600

    fix: show icons in bottomtab and add home screen

---
+++ src/navigation/MainNavigator.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AddRecipe, FavoritesScreen, HomeScreen, ProfileScreen, SearchScreen } from '../screens/Main';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const MainStack = createBottomTabNavigator();
+
+export function MainLayout() {
+  return (
+    <MainStack.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Search') {
+            iconName = focused ? 'search' : 'search-outline';
+          } else if (route.name === 'Add') {
+            iconName = focused ? 'add-circle' : 'add-circle-outline';
+          } else if (route.name === 'Favorites') {
+            iconName = focused ? 'heart' : 'heart-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person-outline';
+          } else if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={24} color={color} />;
+        },
+        tabBarActiveTintColor: '#003566',
+        tabBarInactiveTintColor: '#000814',
+      })}
+    >
+      <MainStack.Screen name="Search" component={SearchScreen} options={{headerShown: false}} />
+      <MainStack.Screen name="Add" component={AddRecipe} options={{headerShown: false}} />
+      <MainStack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
+      <MainStack.Screen name="Favorites" component={FavoritesScreen} options={{headerShown: false}} />
+      <MainStack.Screen name="Profile" component={ProfileScreen} options={{headerShown: false}}/>
+    </MainStack.Navigator>
+  );
+}
--- src/screens/Main/index.ts
+++ src/screens/Main/index.ts
@@ -2,3 +2,4 @@ export {AddRecipe} from './AddRecipe';
 export {FavoritesScreen} from './FavoritesScreen';
 export {ProfileScreen} from './ProfileScreen';
 export {SearchScreen} from './SearchScreen';
+export {HomeScreen} from './HomeScreen';
