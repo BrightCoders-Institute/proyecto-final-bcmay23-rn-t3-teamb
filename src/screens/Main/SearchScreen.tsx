ed739e040c14f4436731bffb322627a7680c15e7
Alfredo20164920 <apaz20164920@gmail.com>
2023-08-17 20:06:22 -0600

    feat: Redirect if user is login

--- src/screens/Main/SearchScreen.tsx
+++ src/screens/Main/SearchScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Button, Text, View } from 'react-native'
+import auth from '@react-native-firebase/auth';
 
 export const SearchScreen = () => {
   return (
     <View>
       <Text>SearchScreen</Text>
+      <Button title='LogOut' onPress={() => auth().signOut()}/>
     </View>
   )
 }
